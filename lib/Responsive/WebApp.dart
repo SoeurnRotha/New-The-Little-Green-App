939718c44c1e4ff39e4733b134b272ee963c526f
SoeurnRotha <kl195070@gmail.com>
2022-11-17 22:29:37 +0700

    done

--- lib/Responsive/WebApp.dart
+++ lib/Responsive/WebApp.dart
@@ -1,16 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-import 'package:responsive_builder/responsive_builder.dart';
-import 'package:the_little_green_product_e_com/Responsive/Web/About%20Us/AboutUs_Mobile.dart';
 import 'package:the_little_green_product_e_com/Responsive/Web/Product/Custom_Scrool.dart';
-import 'package:the_little_green_product_e_com/Responsive/Web/Responsive_WebApp.dart';
-import 'package:the_little_green_product_e_com/Responsive/Web/Route/AppRouter.dart';
-
 import 'Web/Home/Home.dart';
-import 'Web/Responsive_MoblieApp.dart';
-import 'Web/Responsive_TebletApp.dart';
 
 class WebApp extends StatefulWidget {
   const WebApp({Key? key}) : super(key: key);
